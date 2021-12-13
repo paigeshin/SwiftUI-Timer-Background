1ceb661958b6aa739b2ef870643f24235f45c296
paige <paige@paiges-Mac-mini.local>
2021-12-13 22:10:03 +0900

    complete

---
+++ SwiftBackgroundTaskTimer/SwiftBackgroundTaskTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBackgroundTaskTimerApp.swift
+//  SwiftBackgroundTaskTimer
+//
+//  Created by paige on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBackgroundTaskTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
